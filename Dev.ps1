7d3a01a945f45a793e2a73a41f6a0fbeec8b8a4d
Page Brooks <pagebrooks@users.noreply.github.com>
2015-09-22 21:43:32 -0700

    Update Dev.ps1

--- Dev.ps1
+++ Dev.ps1
@@ -1,6 +1,3 @@
-$Boxstarter.RebootOk=$true # Allow reboots?
-$Boxstarter.NoPassword=$false # Is this a machine with no login password?
-$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
 
 # Windows Configuration
 Update-ExecutionPolicy RemoteSigned
