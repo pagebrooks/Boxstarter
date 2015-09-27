a45448504897167e02cc82614bef3c5d0379bf87
Page Brooks <pagebrooks@users.noreply.github.com>
2015-09-26 23:21:33 -0700

    Update Dev.ps1

--- Dev.ps1
+++ Dev.ps1
@@ -1,19 +1,22 @@
-  $vsIsoPath = 'z:\dev\SW_DVD5_Visual_Studio_Pro_2012_English_Core_MLF_X18-35900.iso'
-
-
+  $vs2012IsoPath = '\\vmware-host\Shared Folders\DEV\SW_DVD5_Visual_Studio_Pro_2012_English_Core_MLF_X18-35900.ISO'
+  $vs2012AdminDeploymentFile = "https://raw.github.com/pagebrooks/Boxstarter/master/VS2012-AdminDeployment.xml"
+  
   function Mount-DiskImageReturnDriveLetter($imagePath) { 
     Write-Host "mounting iso at: $imagePath"
      $vcdmount = "$($Boxstarter.programFiles86)\Elaborate Bytes\VirtualCloneDrive\vcdmount.exe"
-     Start-ChocolateyProcessAsAdmin -statements `"$imagePath`" -exeToRun `"$vcdmount`"
+     $args = "/l=E `"$imagePath`""
+     Start-ChocolateyProcessAsAdmin -statements $args -exeToRun `"$vcdmount`"
      write-host "waiting 10 seconds"
      start-sleep -s 10
-     return "d"
+     return "E"
   }
 
   function Dismount-DiskImage($imagePath) {
       try { 
+          Write-Host "dismounting iso: $imagePath"
           $vcdmount = "$($Boxstarter.programFiles86)\Elaborate Bytes\VirtualCloneDrive\vcdmount.exe"
-          Start-ChocolateyProcessAsAdmin -statements /u -exeToRun $vcdmount
+          $args = "/u"
+          Start-ChocolateyProcessAsAdmin -statements $args -exeToRun $vcdmount
           write-host "waiting 10 seconds"
           start-sleep -s 10
       }
@@ -32,29 +35,36 @@
   }
 }
 
-
-function Install-VisualStudio2012([string]$vsinstaller) { 
-    $vsadminFile = "$env:temp\admindeployment.xml"
-    $client = New-Object System.Net.WebClient;
-    $client.DownloadFile("https://raw.github.com/pagebrooks/Boxstarter/master/VS2012-AdminDeployment.xml", $vsadminFile);
-    
+function Install-VisualStudio2012() { 
+  
     $devenvPath = "$($Boxstarter.programFiles86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
-    if((Test-Path $devenvPath) -eq $false) {
-      Write-Host "Installing Visual Studio 2012 as it is not already on path $devenvPath"
+    if((Test-Path $devenvPath) -eq $false) {   
       
+      $drive = Mount-DiskImageReturnDriveLetter $vs2012IsoPath
+      Write-Host "Downloading VS2012-AdminDeployment.xml"
+      $vsAdminFile = "$env:temp\VS2012-AdminDeployment.xml"
+      $client = New-Object System.Net.WebClient;
+      $client.DownloadFile($vs2012AdminDeploymentFile, $vsAdminFile);
+      
+      Write-Host "Installing VS2012 as it is not already on path $devenvPath"
+      $vsInstaller = "${drive}:\vs_professional.exe"
       $vsargs = "/Passive /NoRestart /AdminFile $vsadminFile /Log $env:temp\vs.log"
-      Start-ChocolateyProcessAsAdmin -statements $vsargs -exeToRun $vsinstaller
+      Start-ChocolateyProcessAsAdmin -statements $vsargs -exeToRun $vsInstaller
+      Dismount-DiskImage $vsIsoPath -ErrorAction SilentlyContinue
       Reboot-IfRequired
-    }
-    else { 
+    } else { 
       Write-Host "VS2012 already installed as devenv.exe found on path $devenvPath"
     }
 
     if((Get-Item "$($Boxstarter.programFiles86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe").VersionInfo.ProductVersion -lt "11.0.60115.1") {
-      Install-ChocolateyPackage 'vs update 4' 'exe' '/passive /norestart' 'http://download.microsoft.com/download/D/4/8/D48D1AC2-A297-4C9E-A9D0-A218E6609F06/VSU4/VS2012.4.exe'
+      $vsUpdate4Path = 'http://download.microsoft.com/download/D/4/8/D48D1AC2-A297-4C9E-A9D0-A218E6609F06/VSU4/VS2012.4.exe'
+      Install-ChocolateyPackage 'VS2012 Update 4' 'exe' '/passive /norestart' $vsUpdate4Path
       Reboot-IfRequired
+    } else {
+      Write-Host "VS2012 Update 4 already installed, skipping"
     }
 }
+
 
 try {
     
@@ -66,73 +76,54 @@ Disable-UAC
 Disable-InternetExplorerESC
 Enable-RemoteDesktop
 
-if(!(Test-Path -Path "$env:SystemDrive\Projects")){
-    md C:\Projects
-}
-
-#choco install TelnetClient -source windowsFeatures -y
-#choco install IIS-WebServerRole -source windowsfeatures -y
-#choco install IIS-HttpCompressionDynamic -source windowsfeatures -y
-#choco install IIS-ManagementScriptingTools -source windowsfeatures -y
-#choco install IIS-WindowsAuthentication -source windowsfeatures -y
-
 choco install VirtualCloneDrive -y
+Install-VisualStudio2012
+choco install webpi -y
+choco install resharper -y
 
-# Visual Studio Install
-
-try {
-      $drive = Mount-DiskImageReturnDriveLetter $vsIsoPath
-      Install-VisualStudio2012 "${drive}:\vs_professional.exe"
-}
-finally {
-    Dismount-DiskImage $iso -ErrorAction SilentlyContinue
-}
-
-#choco install imdisk -y
-#choco install psget -y
-#choco install hipchat -y
-#choco install 7Zip -y
-#choco install NugetPackageExplorer -y
-#choco install conemu -y
-#choco install greenshot -y
-#choco install fiddler4 -y
-#choco install curl -y
+choco install psget -y
+choco install hipchat -y
+choco install 7Zip -y
+choco install NugetPackageExplorer -y
+choco install conemu -y
+choco install greenshot -y
+choco install fiddler4 -y
+choco install curl -y
 choco install notepadplusplus.install -y
-#choco install gow -y
-#choco install regexpixie -y
-#choco install linqpad -y
-#choco install dotpeek -y
-#choco install winmerge -y
-#choco install windirstat -y
-#choco install filezilla -y
-#choco install paint.net -y
-#choco install adobereader -y
+choco install gow -y
+choco install regexpixie -y
+choco install linqpad -y
+choco install dotpeek -y
+choco install winmerge -y
+choco install windirstat -y
+choco install filezilla -y
+choco install paint.net -y
+choco install adobereader -y
 
-#choco install git.install -y
-#choco install poshgit -y
-#choco install git-credential-winstore -Version 1.2.0.0 -y
+choco install git.install -y
+choco install poshgit -y
+choco install git-credential-winstore -Version 1.2.0.0 -y
 
-#choco install VisualStudio2012Professional -InstallArguments "WebTools" -y
-#choco install webpi -y
-#choco install resharper -y
-
-#choco install javaruntime -y
-#choco install golang -y
+choco install javaruntime -y
+choco install golang -y
 
 # Fix SSH-Agent error by adding the bin directory to the `Path` environment variable
-#$env:PSModulePath = $env:PSModulePath + ";${Env:ProgramFiles(x86)}\Git\bin"
-
+$env:PSModulePath = $env:PSModulePath + ";${Env:ProgramFiles(x86)}\Git\bin"
 
 #Install-ChocolateyPinnedTaskBarItem "$env:SystemRoot\system32\WindowsPowerShell\v1.0\powershell.exe"  
 #Install-ChocolateyPinnedTaskBarItem "${Env:ProgramFiles(x86)}\Notepad++\notepad++.exe"
 #Install-ChocolateyFileAssociation ".build" "${Env:ProgramFiles(x86)}\Notepad++\notepad++.exe"
 #Install-ChocolateyFileAssociation ".config" "${Env:ProgramFiles(x86)}\Notepad++\notepad++.exe"
 
-#REG ADD "HKCU\Software\Microsoft\Internet Explorer\Main" /V "Start Page" /D "http://www.google.com/" /F
+choco install Firefox -y
+choco install GoogleChrome -y
+Reboot-IfRequired
 
-#choco install Firefox -y
-#choco install GoogleChrome -y
-#install-module -ModuleUrl https://github.com/pagebrooks/BoxStarter/raw/master/mount.iso.psm1
+choco install TelnetClient -source windowsFeatures -y
+choco install IIS-WebServerRole -source windowsfeatures -y
+choco install IIS-HttpCompressionDynamic -source windowsfeatures -y
+choco install IIS-ManagementScriptingTools -source windowsfeatures -y
+choco install IIS-WindowsAuthentication -source windowsfeatures -y
 
 #Enable-MicrosoftUpdate
 #Install-WindowsUpdate -AcceptEula
